592f0e132fee63c1197066a80b3d749421abb7ba
yb123speed <904896209@qq.com>
2019-05-26 00:21:15 +0800

    Db configuration and some db actions

--- tool/db/db.go
+++ tool/db/db.go
@@ -8,10 +8,38 @@ import(
 	"fmt"
 )
 
-type Struct DbConn{
+type DbConn struct{
 	Db *sql.DB
 }
 
 func InitDbConn() (*DbConn, error) {
-	
+	dbconn := viper.GetString("Database.DbConn")
+	db, err := sql.Open("mysql", dbconn)
+	if err != nil {
+		return nil, fmt.Errorf("get db connection error")
+	}
+	maxOpenConns := viper.GetInt("Database.MaxOpenConn")
+	maxIdleConns := viper.GetInt("Database.MaxIdleConn")
+	maxLifetime := viper.GetInt("Database.MaxLifetime")
+
+	// TODO Configure DB Service
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, fmt.Errorf("connect to db error")
+	} else {
+		dbconn := &DbConn{db}
+		return dbconn, nil
+	}
 }
+
+func (c *DbConn) Insert(sql string, args ...interface{}) (lastInsertId int64, err error) {
+	res, err := c.Db.Exec(sql, args...)
+	if err != nil {
+		return
+	}
+	return res.LastInsertId()
+}
+
+// TODO  code other db actions
